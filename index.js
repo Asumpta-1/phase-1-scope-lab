2a26aab5246060d8f6055358b2bbcf81cdf2c830
asumpta <sumgathoni@gmail.com>
2023-01-14 01:06:02 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName.toUpperCase;
+}
+
+// function setBestCustomer() {
+//     var bestCustomer = 'not bob';
+// }
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+  }
+
+  const leastFavoriteCustomer = 'def not bob';
+
+  function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'maybe jane';
+  }
